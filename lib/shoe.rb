968fa8685f603313d0e4c3e1c72c462248468e6c
Mason Phillips <mjphillips2000@gmail.com>
2020-10-29 08:07:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-      if brand != BRAND.each
-        BRAND << brand
-      end
+    BRAND << brand unless BRANDS.include?(brand)
   end
 
   def cobble
